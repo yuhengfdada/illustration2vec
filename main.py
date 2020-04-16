56d02d204b21b8248ccd181df2caa9fe2c702270
KikiDMW <yizhezhao@foxmail.com>
2020-04-16 17:56:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,94 @@ illust2vec = i2v.make_i2v_with_chainer(
 # illust2vec = i2v.make_i2v_with_caffe(
 #     "illust2vec_tag.prototxt", "illust2vec_tag_ver200.caffemodel",
 #     "tag_list.json")
+
+output = {}
+
+#output is a dictionary in the format like:
+#{"1.image" : ["blonde hair", "blue eyes", "long hair", "smile"], 
+# "2.image" : ["brown hair", "green eyes", "short hair", "ribbon"]}
+count = 0
 for i in range(1,38159):
-    img = Image.open("{}.jpg".format())
+    img = Image.open("{}.jpg".format(i))
     result = illust2vec.estimate_plausible_tags([img], threshold=0.25)
     result_dict = dict(result[0]['general'])
-    print(wow)
+    #print(result_dict)
     if '1girl' in result_dict:
-        for key in result_dict:
+        count += 1
+
+        hair = None
+        p = 0
+        for j in hair_list:
+            if result_dict.get(j, 0) > p:
+                hair = j
+                p = result_dict[j]
+        lableList = [hair]
+        
+        eye = None
+        p = 0
+        for j in eye_list:
+            if result_dict.get(j, 0) > p:
+                eye = j
+                p = result_dict[j]
+        lableList.append(eye)
+
+        hairstyle = None
+        p = 0
+        for j in hairstyle_list:
+            if result_dict.get(j, 0) > p:
+                hairstyle = j
+                p = result_dict[j]
+        lableList.append(hairstyle)
+
+        for j in other_list:
+            if j in result_dict:
+                lableList.append(j)
+        
+        output.update({"i.jpg" : lableList})
+
+
+#The name of the output file
+filename = ""
+f = open(filename, "w")
+
+print(count, file = f)
+
+for i in hair_list:
+    print(i, end = ' ', file = f)
+for i in eye_list:
+    print(i, end = ' ', file = f)
+for i in hairstyle_list:
+    print(i, end = ' ', file = f)
+for i in other_list:
+    print(i, end = ' ', file = f)
+print()
+
+for i in range(1, 38159):
+    imageName = "{}.jpg".format(i)
+    if imageName in output:
+        print(imageName, end = " ", file = f)
+        
+        for j in hair_list:
+            if j in output.get(imageName):
+                print(" 1", end = " ", file = f)
+            else:
+                print("-1", end = " ", file = f)
+        
+        for j in eye_list:
+            if j in output.get(imageName):
+                print(" 1", end = " ", file = f)
+            else:
+                print("-1", end = " ", file = f)
+
+        for j in hairstyle_list:
+            if j in output.get(imageName):
+                print(" 1", end = " ", file = f)
+            else:
+                print("-1", end = " ", file = f)
+
+        for j in other_list:
+            if j in output.get(imageName):
+                print(" 1", end = " ", file = f)
+            else:
+                print("-1", end = " ", file = f)
+        print()
