979f32ad5795cf9118e3c98581b8eedb9bb212bc
KikiDMW <yizhezhao@foxmail.com>
2020-04-16 19:38:24 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ output = {}
 #output is a dictionary in the format like:
 #{"1.image" : ["blonde hair", "blue eyes", "long hair", "smile"], 
 # "2.image" : ["brown hair", "green eyes", "short hair", "ribbon"]}
+n = 5
 count = 0
-for i in range(1,38159):
-    img = Image.open("{}.jpg".format(i))
+for i in range(1, n):
+    img = Image.open("/localdata/yzhaocj/lable/illustration2vec-master/images/{}.jpg".format(i))
     result = illust2vec.estimate_plausible_tags([img], threshold=0.25)
     result_dict = dict(result[0]['general'])
-    #print(result_dict)
+#    print(result_dict)
     if '1girl' in result_dict:
         count += 1
 
@@ -54,33 +55,33 @@ for i in range(1,38159):
             if j in result_dict:
                 lableList.append(j)
         
-        output.update({"i.jpg" : lableList})
-
+        output.update({"{}.jpg".format(i) : lableList})
+#print(output)
 
 #The name of the output file
-filename = ""
+filename = "test.txt"
 f = open(filename, "w")
 
 print(count, file = f)
 
 for i in hair_list:
-    print(i, end = ' ', file = f)
+    print(i.replace(' ', '_'), end = ' ', file = f)
 for i in eye_list:
-    print(i, end = ' ', file = f)
+    print(i.replace(' ', '_'), end = ' ', file = f)
 for i in hairstyle_list:
-    print(i, end = ' ', file = f)
+    print(i.replace(' ', '_'), end = ' ', file = f)
 for i in other_list:
-    print(i, end = ' ', file = f)
-print()
+    print(i.replace(' ', '_'), end = ' ', file = f)
+print("", file = f)
 
-for i in range(1, 38159):
+for i in range(1, n):
     imageName = "{}.jpg".format(i)
     if imageName in output:
         print(imageName, end = " ", file = f)
         
         for j in hair_list:
             if j in output.get(imageName):
-                print(" 1", end = " ", file = f)
+               print(" 1", end = " ", file = f)
             else:
                 print("-1", end = " ", file = f)
         
@@ -101,4 +102,4 @@ for i in range(1, 38159):
                 print(" 1", end = " ", file = f)
             else:
                 print("-1", end = " ", file = f)
-        print()
+        print("", file = f)
